e4c206ca460b0470e1250ac9b1d690b74c64edc5
Nihad <nihatus353@gmail.com>
2024-02-16 20:44:13 +0400

    Добавьте файлы проекта.

---
+++ websitebusinesscard.client/src/App.jsx
@@ -0,0 +1,23 @@
+﻿import './App.css';
+
+function App() {
+
+    const contents =
+        <div className="contents">
+            <div className="top_content">
+                <div className="circle">
+                    <button>СМОТРЕТЬ<br/>ПОРТФОЛИО</button>
+                </div>
+            </div>
+
+        </div>
+    
+
+    return (
+        <div>
+            {contents}
+        </div>
+    );
+}
+
+export default App;
---
+++ websitebusinesscard.client/src/components/fragments/Header.jsx
@@ -0,0 +1,27 @@
+﻿import React from "react";
+import "./Header.css";
+function Header() {
+
+    const logoUrl = '/images/logo.png';
+
+    return (
+        <header>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-3 logo">
+                    <img src={logoUrl} alt="Logo" />
+                    </div>
+                    <div className="col-md-8 top_menu">
+                        <a href="#" className="topnav_link">УСЛУГИ</a>
+                        <a href="#" className="topnav_link">ОТЗЫВЫ</a>
+                        <a href="#" className="topnav_link">ПОРТФОЛИО</a>
+                        <a href="#" className="topnav_link">КОНТАКТЫ</a>
+                        <a href="tel:+994505725294" className="header__tel">+994 50 572 52 94</a>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
